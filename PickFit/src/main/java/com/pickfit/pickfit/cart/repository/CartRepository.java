bc18cdd44fd88bc0cdd41d9b84682137c7a26d7f
JJiwonn <163959530+JJiwonn@users.noreply.github.com>
2024-12-03 16:15:05 +0900

    Mypage 완료 commit
    Mypage 완료 commit

--- PickFit/src/main/java/com/pickfit/pickfit/cart/repository/CartRepository.java
+++
@@ -1,7 +0,0 @@
-package com.pickfit.pickfit.cart.repository;
-
-import com.pickfit.pickfit.cart.entity.CartItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CartRepository extends JpaRepository <CartItem, Long>{
-}
